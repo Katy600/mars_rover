c4630c95afc2c3d9ef4aef3791c5c55f4e157d8a
Katy McCann <katherine.mccann@digital.justice.gov.uk>
2021-07-02 20:04:33 +0100

    all tests passing

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,10 +1,8 @@
 
 class MarsRover
-  attr_reader :plateau_size
+  attr_reader :plateau_size, :current_coordinates, :current_direction
 
-  def initialize
-    print "Please input your Rovers coordinates"
-  end
+  COMPASS_POINTS = ['N', 'E', 'S', 'W']
 
   def set_plateau_size(coordinates)
     @plateau_size = coordinates.split(' ').map(&:to_i)
@@ -12,7 +10,19 @@ class MarsRover
 
   def set_position(position)
     raise_error_message(position)
-    given_coordinates(position) + initial_direction(position)
+  end
+
+  def instructions(direction)
+    current_direction = COMPASS_POINTS.index(@current_direction)
+    if direction == 'L'
+      @current_direction = COMPASS_POINTS[current_direction - 1]
+    else
+      @current_direction = COMPASS_POINTS[current_direction + 1]
+    end
+  end
+
+  def final_position
+    @current_coordinates.join(' ') + ' ' + @current_direction
   end
 
   private
@@ -23,33 +33,31 @@ class MarsRover
         raise StandardError.new 'Invalid: Please enter an x, y and a direction'
       when incorrect_letter?(position)
         raise StandardError.new 'Invalid direction given: Please enter N, E, S or W, precided by two digits'
-      when out_side_plateau?(position)
+      when out_side_plateau?
         raise StandardError.new 'Invalid: the position must not be greater than the size of the plateau'
     end
   end
 
-  def given_coordinates(position)
-    initial_coordinates(position).map(&:to_i)
-  end
-
-  def out_side_plateau?(position)
-    @plateau_size.zip(given_coordinates(position)).map { |plateau_size, given_coordinates| true if given_coordinates > plateau_size}.include?(true)
-  end
-
-  def initial_direction(position)
+  def initial_compass_direction(position)
+    @current_direction = [position.split(" ")[2]].shift
     [position.split(" ")[2]]
   end
 
   def initial_coordinates(position)
+    @current_coordinates = position.split(" ").shift(2).map(&:to_i)
     position.split(" ").shift(2)
   end
 
+  def out_side_plateau?
+    @plateau_size.zip(@current_coordinates).map { |plateau_size, given_coordinates| true if given_coordinates > plateau_size}.include?(true)
+  end
+
   def wrong_order?(position)
-    ['N', 'E', 'S', 'W'].difference(initial_coordinates(position)).size < 4
+    COMPASS_POINTS.difference(initial_coordinates(position)).size < 4
   end
 
   def incorrect_letter?(position)
-    ['N', 'E', 'S', 'W'].difference(initial_direction(position)).size == 4
+    COMPASS_POINTS.difference(initial_compass_direction(position)).size == 4
   end
 
   def wrong_number_of_digits(position)
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -2,13 +2,14 @@
 require 'mars_rover'
 
 describe MarsRover do
-
+  new_game = MarsRover.new
+  new_game.set_plateau_size('5 5')
   describe '#plateau_size' do
     context 'when a user sets up a new game' do
       new_game = MarsRover.new
       it 'can set the size of the plateau' do
         new_game.set_plateau_size('5 5')
-        expect(new_game.plateau_size).to eq([5,5])
+        expect(new_game.plateau_size).to eq([5, 5])
       end
     end
   end
@@ -21,45 +22,105 @@ describe MarsRover do
   #   end
   # end
   #
-  describe '#set_position'do
-    new_game = MarsRover.new
-    new_game.set_plateau_size('5 5')
+  describe '#set_position (error messages)'do
 
-    context 'user sets the position correctly' do
-      it 'accepts it' do
-        expect(new_game.set_position('1 2 N')).to eq [1,2,'N']
-      end
-    end
-
-    context 'user sets too many digits or letters' do
+    context 'when a user sets too many digits or letters' do
       it 'errors' do
         expect {new_game.set_position('1 2 N S')}.to raise_error('Invalid: Please enter an x, y and a direction')
       end
     end
 
-    context 'user sets too few digits or letters' do
+    context 'when a user sets too few digits or letters' do
       it 'errors' do
         expect {new_game.set_position('1 2')}.to raise_error('Invalid: Please enter an x, y and a direction')
       end
     end
 
-    context 'user puts the coordinates in the wrong order' do
+    context 'when a user puts the coordinates in the wrong order' do
       it 'errors' do
         expect {new_game.set_position('N 2 1')}.to raise_error('Invalid: Please enter an x, y and a direction')
         expect {new_game.set_position('2 N 1')}.to raise_error('Invalid: Please enter an x, y and a direction')
       end
     end
 
-    context 'user puts in incorrect letters' do
+    context 'when a user enters incorrect letters' do
       it 'errors' do
         expect {new_game.set_position('2 1 F')}.to raise_error('Invalid direction given: Please enter N, E, S or W, precided by two digits')
       end
     end
 
-    context 'user enters a position outside of the size of the plateau' do
+    context 'when a user enters a position outside of the size of the plateau' do
       it 'errors' do
         expect {new_game.set_position('6 1 N')}.to raise_error('Invalid: the position must not be greater than the size of the plateau')
         expect {new_game.set_position('1 6 N')}.to raise_error('Invalid: the position must not be greater than the size of the plateau')
+      end
+    end
+
+  end
+
+  describe '#instructions' do
+    context 'when a user moves L from N position' do
+      before do
+        new_game.set_position('1 2 N')
+        new_game.instructions('L')
+      end
+      it 'direction becomes W' do
+        expect(new_game.final_position).to eq '1 2 W'
+      end
+    end
+
+    context 'when a user moves L from E position' do
+      before do
+        new_game.set_position('1 2 E')
+        new_game.instructions('L')
+      end
+
+      it 'direction becomes N' do
+        expect(new_game.final_position).to eq '1 2 N'
+      end
+    end
+
+    context 'when a user moves L from S position' do
+      before do
+        new_game.set_position('1 2 S')
+        new_game.instructions('L')
+      end
+
+      it 'direction becomes E' do
+        expect(new_game.final_position).to eq '1 2 E'
+      end
+    end
+
+    context 'when a user moves L from W position' do
+      before do
+        new_game.set_position('1 2 W')
+        new_game.instructions('L')
+      end
+
+      it 'direction becomes S' do
+        expect(new_game.final_position).to eq '1 2 S'
+      end
+    end
+
+    context 'when a user moves R from N position' do
+      before do
+        new_game.set_position('1 2 N')
+        new_game.instructions('R')
+      end
+
+      it 'direction becomes E' do
+        expect(new_game.final_position).to eq '1 2 E'
+      end
+    end
+
+    context 'when a user moves R from E position' do
+      before do
+        new_game.set_position('1 2 E')
+        new_game.instructions('R')
+      end
+
+      it 'direction becomes S' do
+        expect(new_game.final_position).to eq '1 2 S'
       end
     end
   end
